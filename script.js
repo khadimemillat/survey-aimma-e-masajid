240aa3008b51f9a0c40c571d4db57da524e10d9e
dailycontest <120779270+wafaqulmasajid@users.noreply.github.com>
2023-01-12 19:02:53 +0530

    made the both same (git & local)

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyLH-08CrRPToQ-Ij3Ncyzuy2AiYnJnIlIiar01raDvrMCjqrApyKaGXrzCQVxz6SeqVw/exec'
 const form = document.forms['submit-to-google-sheet']
 form.addEventListener('submit', e => {
-  e.preventDefault()
+    e.preventDefault()
+    console.log("submitting")
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then(_response => { alert("Message sent successfully, we'll get in touch with you asap (if needed)") })
   form.reset()
