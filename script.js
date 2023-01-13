cb9d20035bc552ffb285ad1d8f5deda8f022e70b
saif <its_secret@mail.com>
2023-01-13 12:28:54 +0530

    1 - Fix typos 2 - Fix index 3 - Ln script to baseFile 4 - ReWrite script file 5 - Use `method=post` instead of `get` 6 - Set `action` to `script file` not external value 7 - Be Happy! It's work Perfectly, Inshallaha ;-)

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyLH-08CrRPToQ-Ij3Ncyzuy2AiYnJnIlIiar01raDvrMCjqrApyKaGXrzCQVxz6SeqVw/exec'
-const form = document.forms['submit-to-google-sheet']
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    console.log("submitting")
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(_response => { alert("Message sent successfully, we'll get in touch with you asap (if needed)") })
-  form.reset()
-    .catch(_error => { alert("Oops something went wron, please try again") })
-})
+console.log("Taaha")
+const form = document.getElementById('submit-to-google-sheet');
+const scriptURL = "https://script.google.com/macros/s/AKfycbyLH-08CrRPToQ-Ij3Ncyzuy2AiYnJnIlIiar01raDvrMCjqrApyKaGXrzCQVxz6SeqVw/exec"
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    fetch(scriptURL, {
+        method: 'POST',
+        body: formData
+    })
+    .then((response) => {
+        if (response.ok) {
+            alert("Message sent successfully, we'll get in touch with you asap (if needed)");
+        } else {
+            alert("Error submitting form. Please try again.");
+        }
+    })      
+
+    // For Developer Taaha! ;)
+    /* Bhai yeh error only (FF) Firefox me execute, uska reason hai CORS, yeh kaafi lamba topic hai,
+    agr padha hoto is link pr jao: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS*/
+
+    .catch((error) => {
+      console.error('Error:', error)
+      alert("Error submitting form. Please try again.");
+    });
+});
